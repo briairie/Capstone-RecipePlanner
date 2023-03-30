28d9e86819706fa60e8408e022a3150091281b73
briairie <birie1@my.westga.edu>
2023-03-29 21:39:47 -0400

    Added shopping list functionality Added shopping list functionality to api and database. Completing shopping list does not deduct from pantry yet.

---
+++ code/backend/RecipePlannerApi/Service/Interface/IShoppingListService.cs
@@ -0,0 +1,12 @@
+﻿using RecipePlannerApi.Model;
+
+namespace RecipePlannerApi.Service.Interface
+{
+    public interface IShoppingListService
+    {
+        List<ShoppingListIngredient> CompleteShoppingList(List<ShoppingListIngredient> list, int userId);
+        void DeleteShoppingListIngredient(int shoppingListId);
+        List<ShoppingListIngredient> GetShoppingList(int userId);
+        ShoppingListIngredient UpsertShoppingListIngredient(ShoppingListIngredient ingredient);
+    }
+}
---
+++ code/backend/recipePlannerDatabase/dbo/Stored Procedures/Shopping List/get_shopping_list_ingredient.sql
@@ -0,0 +1,11 @@
+CREATE PROCEDURE [dbo].[get_shopping_list_ingredient]
+  @shoppingListId INT
+AS
+  SELECT 
+		 [shopping_list_id]   AS ShoppingListId
+		,[user_id]       	  AS UserId
+		,[ingredient_name]    AS IngredientName
+		,[quantity]           AS Quantity
+		,[unit_id]            AS UnitId
+  FROM [dbo].[shopping_list] 
+  WHERE shopping_list_id = @shoppingListId
